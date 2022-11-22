9eb2d7b4e0aeabb69fa3e6b0798c15e6802cee65
Julia132 <uka132@mail.ru>
2022-11-23 03:12:23 +0400

    Feat: logger format changed

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,20 +1,15 @@
 import logging
 
 
-def configure_logging(log_path=None):
+def configure_logging():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
         fmt='%(asctime)s - %(levelname)s - %(message)s',
         datefmt='%d-%b-%y %H:%M:%S'
     )
-    sh = logging.StreamHandler()
-    sh.setLevel(logging.DEBUG)
-    sh.setFormatter(formatter)
-    logger.addHandler(sh)
-    if log_path is not None:
-        fh = logging.FileHandler(log_path)
-        fh.setLevel(logging.DEBUG)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(formatter)
+    logger.addHandler(console)
     return logger
